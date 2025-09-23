63257cab67eef46f6d75b546624ca6aef051db2e
Bonkerstorm <68128602+Bonkerstorm@users.noreply.github.com>
2025-09-23 11:05:42 -0300

    app.js

--- challenge-amigo-secreto_esp-main/app.js
+++ challenge-amigo-secreto_esp-main/app.js
@@ -1,1 +1,44 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+// Array para almacenar los nombres de los amigos
+let amigos = [];
+
+// Agregar amigos
+function agregarAmigo() {
+    let input = document.getElementById("amigo");
+    let nombre = input.value.trim();
+
+    if (nombre === "") {
+        alert("Por favor, inserte un nombre.");
+        return;
+    }
+
+    amigos.push(nombre);
+    input.value = "";
+    actualizarLista();
+}
+
+//  actualizar la lista de amigos
+function actualizarLista() {
+    let lista = document.getElementById("listaAmigos");
+    lista.innerHTML = "";
+
+    for (let i = 0; i < amigos.length; i++) {
+        let li = document.createElement("li");
+        li.textContent = amigos[i];
+        lista.appendChild(li);
+    }
+}
+
+// sortear un amigo secreto
+function sortearAmigo() {
+    if (amigos.length === 0) {
+        alert("No hay amigos disponibles para sortear.");
+        return;
+    }
+
+    let indiceAleatorio = Math.floor(Math.random() * amigos.length);
+    let amigoSorteado = amigos[indiceAleatorio];
+
+    let resultado = document.getElementById("resultado");
+    resultado.innerHTML = `🎉 El amigo secreto es: <strong>${amigoSorteado}</strong>`;
+}
